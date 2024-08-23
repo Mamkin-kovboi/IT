309be276c4f6b78a96786cf695ec8637d78d839a
Dan9 <90175539+Mamkin-kovboi@users.noreply.github.com>
2024-08-23 22:13:56 +0300

    Update Parser binance and bybit.py

--- Parser binance and bybit.py
+++ Parser binance and bybit.py
@@ -3,55 +3,12 @@ import logging
 import datetime
 import asyncio
 import asyncpg
-import psycopg2
+import DB
 from typing import Optional, Dict, Any, List
 
 # Настройка логгирования
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger("logger")
-
-conn = psycopg2.connect(dbname="parser", user="postgres", password="qwerty", host="127.0.0.1")
-cursor = conn.cursor()
-
-# создаем таблицу currency
-cursor.execute(
-    "CREATE TABLE currency (id SERIAL PRIMARY KEY, name VARCHAR(255) NOT NULL,  symbol VARCHAR(255) NOT NULL)")
-conn.commit()
-print("Таблица currency успешно создана")
-
-# создаем таблицу exchanger
-cursor.execute(
-    "CREATE TABLE exchanger (id SERIAL PRIMARY KEY, name VARCHAR(255) NOT NULL,  api_url VARCHAR(255) NOT NULL)")
-conn.commit()
-print("Таблица exchanger успешно создана")
-
-# создаем таблицу currency_pair
-cursor.execute(
-    "CREATE TABLE currency_pair (id SERIAL PRIMARY KEY, currency_from_id INTEGER NOT NULL REFERENCES currency(id),  currency_to_id INTEGER NOT NULL REFERENCES currency(id), exchanger_id INTEGER NOT NULL REFERENCES exchanger(id))")
-conn.commit()
-print("Таблица currency_pair успешно создана")
-
-# создаем таблицу price
-cursor.execute(
-    "CREATE TABLE price (id SERIAL PRIMARY KEY, currency_pair_id INTEGER NOT NULL REFERENCES currency_pair(id),  datetime TIMESTAMP NOT NULL, price FLOAT NOT NULL)")
-conn.commit()
-print("Таблица price успешно создана")
-
-currency = [("Bitcoin", "BTC"), ("Ethereum", "ETH"), ("Solana", "SOL"), ("Dogecoin", "DOGE"), ("Pepe", "PEPE")]
-cursor.executemany("INSERT INTO currency (name, symbol) VALUES (%s, %s)", currency)
-
-conn.commit()
-print("Данные в таблицу Currency добавлены")
-
-exchanger = [("Binance", "https://api.binance.com/api/v3/ticker/price"),
-             ("Bybit", "https://api.bybit.com/spot/v3/public/quote/ticker/price")]
-cursor.executemany("INSERT INTO exchanger (name, api_url) VALUES (%s, %s)", exchanger)
-
-conn.commit()
-print("Данные в таблицу Exchanger добавлены")
-
-
-PARAMS = ("BTCUSDT", "ETHUSDT", "BNBUSDT", "XRPUSDT", "DOGEUSDT")
 
 binance_url = "https://api.binance.com/api/v3/ticker/price"
 bybit_url = "https://api.bybit.com/spot/v3/public/quote/ticker/price"
@@ -73,7 +30,7 @@ async def binance_price(session: aiohttp.ClientSession, pair: str) -> Optional[D
     async with session.get(binance_url, params=params) as response:
         if response.status == 200:
             result = await response.json()
-            logger.info(f"Получена цена для {pair} с Binance: {result["price"]}")
+            logger.info(f"Получена цена для {pair} с Binance: {result['price']}")
             return result
         else:
             error_response = await response.json()
@@ -95,21 +52,15 @@ async def bybit_price(session: aiohttp.ClientSession, pair: str) -> Optional[Dic
     async with session.get(binance_url, params=params) as response:
         if response.status == 200:
             result = await response.json()
-            logger.info(f"Получена цена для {pair} с Bybit: {result["price"]}")
+            logger.info(f"Получена цена для {pair} с Bybit: {result['price']}")
             return result
         else:
             error_response = await response.json()
             logger.error(f"Ошибка Bybit: {response.status} - {error_response}")
             return None
 
-# создаем таблицу cryptocurrency
-cursor.execute(
-    "CREATE TABLE cryptocurrency (id SERIAL PRIMARY KEY, symbol VARCHAR(255) NOT NULL,  exchanger VARCHAR(255) NOT NULL, price INTEGER NOT NULL, datetime TIMESTAMP NOT NULL)")
-conn.commit()
-print("Таблица cryptocurrency успешно создана")
 
-
-async def price() -> None:
+async def price(pairs: List[str]) -> None:
     """Запрашивает цены на криптовалюты с бирж Binance и Bybit и сохраняет их в текстовый файл.
 
     Returns:
@@ -118,18 +69,18 @@ async def price() -> None:
     async with aiohttp.ClientSession() as session:
         while True:
             prices: List[str] = []
-            for pair in PARAMS:
+            for pair in pairs:
                 binance = await binance_price(session, pair)
                 bybit = await bybit_price(session, pair)
 
                 if binance:
                     prices.append(
-                        f"Binance: {pair} - Price: {binance["price"]}  - Time: {datetime.datetime.now()}\n"
+                        f"Binance: {pair} - Price: {binance['price']}  - Time: {datetime.datetime.now()}\n"
                     )
 
                 if bybit:
                     prices.append(
-                        f"Bybit: {pair} - Price: {bybit["price"]}  - Time: {datetime.datetime.now()}\n"
+                        f"Bybit: {pair} - Price: {binance['price']}  - Time: {datetime.datetime.now()}\n"
                     )
 
             if prices:
@@ -140,8 +91,26 @@ async def price() -> None:
             await asyncio.sleep(15)
 
 
-cursor.close()
-conn.close()
+async def fetch_data() -> None:
+    conn = None
+    try:
+        conn = await asyncpg.connect(user='postgres', password='qwerty', database='parser', host='127.0.0.1')
+
+        # SQL-запрос для получения символов валют
+        query = """
+            SELECT currency.name 
+            FROM currency;
+        """
+        rows = await conn.fetch(query)
+        pairs = [row['name'] for row in rows]
+
+        await price(pairs)
+
+    except Exception as e:
+        logger.error(f"Ошибка при получении данных: {e}")
+    finally:
+        if conn:
+            await conn.close()
 
 if __name__ == "__main__":
-    asyncio.run(price())
+    asyncio.run(fetch_data())
