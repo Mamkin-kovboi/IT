ca29f1f08d7105e834f71b03b66269ed1ea05db0
Dan9 <90175539+Mamkin-kovboi@users.noreply.github.com>
2024-09-06 23:22:47 +0300

    Update main.py

--- FastAPI/main/main.py
+++ FastAPI/main/main.py
@@ -1,8 +1,20 @@
+import aiohttp
+import certifi
 from fastapi import FastAPI
 from asyncio import run
+from services import CurrencyService
 
 app = FastAPI()
 currency_service = CurrencyService()
+
+@app.on_event("startup")
+async def startup_event():
+    await currency_service.initialize()
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    await currency_service.close()
+
 
 @app.get("/currencies/")
 async def get_currency_pairs():
@@ -11,3 +23,10 @@ async def get_currency_pairs():
 @app.get("/prices/{currency_pair}")
 async def get_latest_price(currency_pair: str):
     return await currency_service.get_latest_price(currency_pair)
+
+
+
+
+
+
+
