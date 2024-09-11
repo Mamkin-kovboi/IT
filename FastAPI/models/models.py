c02776d12806d1e3b9dddbbe66265e42ebe78204
Dan9 <90175539+Mamkin-kovboi@users.noreply.github.com>
2024-09-11 20:47:31 +0300

    Update models.py

--- FastAPI/models/models.py
+++ FastAPI/models/models.py
@@ -1,30 +1,29 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
 from datetime import datetime
 
 # Модель для валюты
 class Currency(BaseModel):
-    id: int
-    name: str
+    id: int = Field(..., description="Идентификатор валюты")
+    name: str = Field(..., max_length=50, description="Название валюты")
 
 # Модель для цены валюты
 class CurrencyPrice(BaseModel):
-    currency_pair_id: int
-    price: float
-    source: str
+    currency_pair_id: int = Field(..., description="Идентификатор валютной пары")
+    price: float = Field(..., description="Цена", ge=0)
+    source: str = Field(..., max_length=150, description="Название биржи")
     datetime: datetime
 
 # Модель для бирж
 class Exchanger(BaseModel):
-    exchanger_id: int
-    name: str
-    api_url: str
+    exchanger_id: int = Field(..., description="Идентификатор биржи")
+    name: str = Field(..., max_length=50, description="Название биржи")
+    api_url: str = Field(..., max_length=150, description="API биржи")
 
 # Модель для валютных пар
 class CurrencyPair(BaseModel):
-    currency_pair_id: int
-    currency_from_id: int
-    currency_to_id: int
-    exchanger_id: int
-
+    currency_pair_id: int = Field(..., description="Идентификатор валютной пары")
+    currency_from_id: int = Field(..., description="Идентификатор исходной валюты")
+    currency_to_id: int = Field(..., description="Идентификатор целевой валюты")
+    exchanger_id: int = Field(..., description="Идентификатор биржи")
 
