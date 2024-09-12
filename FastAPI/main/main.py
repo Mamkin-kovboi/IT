0bfafc48b00882694a1bf462a0a08cc37fef10d3
Dan9 <90175539+Mamkin-kovboi@users.noreply.github.com>
2024-09-12 22:22:22 +0300

    Update main.py

--- FastAPI/main/main.py
+++ FastAPI/main/main.py
@@ -1,10 +1,13 @@
 import uvicorn
-from services import currencyservice
+from requests import Response
+
+from models import CurrencyPrice
+from services import CurrencyService
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 # Инициализация сервиса
-currency_service = currencyservice()
+currency_service = CurrencyService()
 resource = {}
 
 @asynccontextmanager
@@ -13,15 +16,17 @@ async def app_lifespan(app: FastAPI):
     yield  # Работа между открытием и закрытием
     await currency_service.close()
 
+# Создание приложения FastAPI с lifespan
 app = FastAPI(lifespan=app_lifespan)
 
 @app.get("/")
 async def root():
     return {"message": "Welcome to the currency prices API!"}
 
-@app.get("/prices/{currency_pair}")
+@app.get("/prices/{currency_pair}",response_model=CurrencyPrice)
 async def get_latest_price(currency_pair: str):
     return await currency_service.get_latest_price(currency_pair)
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host='127.0.0.1', port=8000, reload=True)
